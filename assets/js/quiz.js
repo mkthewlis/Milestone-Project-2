7deb14ef84e8b5eeb3bd33edfd46b8d1da87323d
mkthewlis <mkthewlis@gmail.com>
2020-06-21 14:36:15 +0000

    fix: changed the background color functions to one if function instead of two seperate functions. Added a function to increase the results by one if correct answer is selected

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -4,20 +4,20 @@ let results = 0;
 //Only runs function when page is ready
 $(document).ready(function(){
 
+    //Changes background color depending on answer by adding appropriate class
+    $('.quiz-btn').click(function() {
+        if ($(this).hasClass('true')) {
+            $(this).addClass('correct');
+        } else {
+            $(this).addClass('incorrect');
+        }
+    });
 
-    //Adds class to correct answer to change background color - but this only adds one point per turn
+    //Increases user result by only one point if correct answer is selected
     $('.true').one('click', function() {
-        $(this).addClass('correct');
-        //Increases user result by one point
         results ++;
         });
 
 
-
-    //Adds class to incorrect answer to change background color
-    $('.false').click(function(){
-        $(this).addClass('incorrect');
-        });
+        
 });
-
-    
