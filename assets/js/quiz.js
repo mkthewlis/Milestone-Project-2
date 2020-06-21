c1aa8fe5036c197589860064cfc1bbcb12f612bc
mkthewlis <mkthewlis@gmail.com>
2020-06-21 13:08:28 +0000

    feat: began creating quiz feature for project. Added classes to quiz.html to determine if an answer is true or false and made a simple function to console.log this to test link to js script

---
+++ assets/js/quiz.js
@@ -0,0 +1,9 @@
+//Set results at zero at the beginning of the quiz
+let results = 0;
+
+//Only runs function when page is ready
+$(document).ready(function(){
+    $('.false').click(function(){
+        console.log('Test - hello!');
+        })
+    });
