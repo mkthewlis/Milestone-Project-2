ee219832f6d8f6d212abb62f8868d80014adc021
mkthewlis <mkthewlis@gmail.com>
2020-06-21 13:55:22 +0000

    docs: added function to add classes to quiz buttons for correct and incorrect respectively to change their background colors

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -3,7 +3,21 @@ let results = 0;
 
 //Only runs function when page is ready
 $(document).ready(function(){
+
+
+    //Adds class to correct answer to change background color - but this only adds one point per turn
+    $('.true').one('click', function() {
+        $(this).addClass('correct');
+        //Increases user result by one point
+        results ++;
+        });
+
+
+
+    //Adds class to incorrect answer to change background color
     $('.false').click(function(){
-        console.log('Test - hello!');
-        })
-    });
+        $(this).addClass('incorrect');
+        });
+});
+
+    
