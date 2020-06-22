59229033778d5a501605aea5ccd3febd853ced94
mkthewlis <mkthewlis@gmail.com>
2020-06-22 12:47:47 +0000

    docs: tried adding info window to one of the google maps markers to test this functionality. Will now work on making this available to all markers

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,48 +1,60 @@
-function initMap() {
+    function initMap() {
 
-  var northPacific = {lat: 30.400, lng: -171.356};
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 2,
+            center: {lat: 30.400, lng: -171.356}
+        });
 
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: northPacific
-  });
+        //North Pacific Marker
+        var northPacificMarker = new google.maps.Marker({
+            position: new google.maps.LatLng(30.400, -171.356),
+            map: map,
+            title: 'North Pacific Gyre'
+        });
 
-  //North Pacific Marker
-  var northPacificMarker = new google.maps.Marker({
-    position: new google.maps.LatLng(30.400, -171.356),
-    map: map,
-    title: 'North Pacific Gyre'
-  });
+        //South Pacific Marker
+        var southPacificMarker = new google.maps.Marker({
+            position: new google.maps.LatLng(-34.013, -139.597),
+            map: map,
+            title: 'South Pacific Gyre'
+        });
 
-  //South Pacific Marker
-  var southPacificMarker = new google.maps.Marker({
-    position: new google.maps.LatLng(-34.013, -139.597),
-    map: map,
-    title: 'South Pacific Gyre'
-  });
+        //North Atlantic Marker
+        var northAtlanticMarker = new google.maps.Marker({
+            position: new google.maps.LatLng(41.432, -41.885),
+            map: map,
+            title: 'South Pacific Gyre'
+        });
 
-  //North Atlantic Marker
-  var southPacificMarker = new google.maps.Marker({
-    position: new google.maps.LatLng(41.432, -41.885),
-    map: map,
-    title: 'South Pacific Gyre'
-  });
+        //South Atlantic Marker
+        var southAtlanticMarker = new google.maps.Marker({
+            position: new google.maps.LatLng(-28.856, -12.090),
+            map: map,
+            title: 'South Pacific Gyre'
+        });
 
-  //South Atlantic Marker
-  var southPacificMarker = new google.maps.Marker({
-    position: new google.maps.LatLng(-28.856, -12.090),
-    map: map,
-    title: 'South Pacific Gyre'
-  });
+        //Indian Ocean Marker
+        var indianOceanMarker = new google.maps.Marker({
+            position: new google.maps.LatLng(-23.656, 82.567),
+            map: map,
+            title: 'South Pacific Gyre'
+        });
 
-  //South Atlantic Marker
-  var southPacificMarker = new google.maps.Marker({
-    position: new google.maps.LatLng(-23.656, 82.567),
-    map: map,
-    title: 'South Pacific Gyre'
-  });
+        var infoWindowContent = ['North Pacific Gyre','More information can be found at: https://en.wikipedia.org/wiki/North_Pacific_Gyre'];
 
-}
+        var infoWindow = new google.maps.InfoWindow({
+          content: infoWindowContent
+        });
+
+        //Function to add info window to clicked marker
+        northPacificMarker.addListener('click', function() {
+          infoWindow.open(map, northPacificMarker);
+        });
+
+    }
+
+
+
 
 /*
   var northPacific = {lat: 30.400, lng: -171.356};
