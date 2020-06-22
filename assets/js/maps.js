b6c9e8ba6260b4b3a64a0b0487989c300c68f71c
mkthewlis <mkthewlis@gmail.com>
2020-06-22 10:36:32 +0000

    fix: corrected border for Google Maps section. Added coordinates and location of five gyres to maps.js

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,4 +1,23 @@
 function initMap() {
+  var northPacific = {lat: 30.400, lng: -171.356};
+  var southPacific = {lat: -34.013, lng: -139.597};
+  var northAtlantic = {lat: 41.432, lng: -41.885};
+  var southAtlantic = {lat: -28.856, lng: -12.090};
+  var indianOcean = {lat: -23.656, lng: 82.567}; 
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: northPacific
+  });
+
+  var marker = new google.maps.Marker({
+    position: northPacific,
+    map: map,
+    title: 'North Pacific Gyre'
+  });
+}
+
+/*
   var myLatLng = {lat: -25.363, lng: 131.044};
 
   var map = new google.maps.Map(document.getElementById('map'), {
@@ -11,6 +30,7 @@ function initMap() {
     map: map,
     title: 'Hello World!'
   });
-}
+
+  */
 
 // Source: https://developers.google.com/maps/documentation/javascript/adding-a-google-map
