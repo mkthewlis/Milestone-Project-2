0d36f2bd4dbf0dddb00a4917227c3fa05d6c2c6f
mkthewlis <mkthewlis@gmail.com>
2020-06-28 10:33:47 +0000

    fix: ran all files through respective code validators. This lead to adding missing semi-colons to quiz.js and arrows.js as they were missing on some lines of the functions. The validator also helped me add a missing header element to index.html

--- assets/js/arrows.js
+++ assets/js/arrows.js
@@ -3,13 +3,13 @@ $(document).ready(function(){
 
 //Smooth scroll by matching href to id of destination
     $('a[href*="#"]').on('click', function(e) {
-        e.preventDefault()
+        e.preventDefault();
 
         $('html, body').animate({
             scrollTop: $($(this).attr('href')).offset().top,
             //Speed set to 700 milliseconds across all pages
-            }, 700, 'linear')
-        })
+            }, 700, 'linear');
+        });
     });
 
 // Source code: https://www.taniarascia.com/smooth-scroll-to-id-with-jquery/
--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -34,13 +34,13 @@ $(document).ready(function(){
         //States user's score with personalised prompt to try again
         $('#results').text('Your score is: ' + results + '/7 points');
         if (results === 7){
-            $('.try-again').text(`You're an expert! Why not test someone else next?`)
+            $('.try-again').text(`You're an expert! Why not test someone else next?`);
         } else if (results > 5){
-            $('.try-again').text('Well done! Why not test someone else next?')
+            $('.try-again').text('Well done! Why not test someone else next?');
         } else if (results < 5 && results >= 1){
-            $('.try-again').text('Good effort! Would you like try again?')
+            $('.try-again').text('Good effort! Would you like try again?');
         } else {
-            $('.try-again').text('It looks like something went wrong! Why not try again?')
+            $('.try-again').text('It looks like something went wrong! Why not try again?');
         }
     });
 
