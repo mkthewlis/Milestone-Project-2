4b8575b1cfd3d6a59c9c9dc40a6a35cd57be79b4
mkthewlis <mkthewlis@gmail.com>
2020-07-02 06:08:38 +0000

    fix: corrected modal function by removing unnecessary event listener, so it only opens when the form is submitted

--- assets/js/newsletter.js
+++ assets/js/newsletter.js
@@ -1,4 +1,3 @@
-
     function newsletterSignUp(contactForm) {
         //Uses template from my EmailJS account and data input by users
         emailjs.send("gmail", "our_oceans_our_plastic", {
@@ -11,9 +10,7 @@
                 //Adds thank you message to modal with user's name
                 $(".thank-you-message").text("Great to have you with us " + contactForm.firstname.value + "! Look out for our newsletter in your inbox soon.");
                 //Only opens modal on successful form submission
-                $("#newsletter-form").submit(function() {
-                    $("thankYouModal").modal("toggle");
-                });
+                    $("#thankYouModal").modal("toggle");
                 //Refreshes form after user closes modal to remove content
                 $("#close-btn").click(function(){
                     location.reload();
@@ -25,7 +22,6 @@
         );
         return false;  // To block from loading a new page
     }
-
 
 /* Source of guidance used to create this code:
 Code Institute Mini Project: https://github.com/mkthewlis/rosie-resume-with-js/blob/master/assets/js/sendEmail.js 
